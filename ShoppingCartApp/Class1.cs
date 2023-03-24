57eca7edb31bf95098d9f7de7d4b8a76d990e262
lab_services_student <lab_services_student@lab000000>
2023-03-24 11:49:30 +0000

    Add project files.

---
+++ ShoppingCartApp/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApp
+{
+    internal class Cart
+    {
+        private List<Item> items;
+
+        public Cart()
+        {
+            items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+            Console.WriteLine("Added " + item.Name + " to cart.");
+        }
+
+        public void RemoveItem(string itemName)
+        {
+            Item itemToRemove = items.Find(item => item.Name == itemName);
+            if (itemToRemove != null)
+            {
+                items.Remove(itemToRemove);
+                Console.WriteLine("Removed " + itemName + " from cart.");
+            }
+            else
+            {
+                Console.WriteLine("Item not found in cart.");
+            }
+        }
+
+        public void DisplayCart()
+        {
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Cart is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Items in cart:");
+                foreach (Item item in items)
+                {
+                    Console.WriteLine("- " + item.Name + " ($" + item.Price + ")");
+                }
+                Console.WriteLine("Total: $" + GetTotal());
+            }
+        }
+
+        public double GetTotal()
+        {
+            double total = 0;
+            foreach (Item item in items)
+            {
+                total += item.Price;
+            }
+            return total;
+        }
+
+        public void ClearCart()
+        {
+            items.Clear();
+            Console.WriteLine("Cart cleared.");
+        }
+    }
+}
---
+++ ShoppingCartApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using ShoppingCartApp;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cart cart = new Cart();
+
+        // Add some items to the cart
+        cart.AddItem(new Item("Book", 10.99));
+        cart.AddItem(new Item("T-Shirt", 19.99));
+        cart.AddItem(new Item("Headphones", 29.99));
+
+        // Display the cart
+        cart.DisplayCart();
+
+        // Remove an item from the cart
+        cart.RemoveItem("T-Shirt");
+
+        // Display the updated cart
+        cart.DisplayCart();
+
+        // Clear the cart
+        cart.ClearCart();
+    }
+}
+
